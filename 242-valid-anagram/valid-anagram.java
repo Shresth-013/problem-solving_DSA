6a183955c7e4fbedc97e30fa4f3dbe2ae33ec189
Shresth Raj <Shresthraj12004@gmail.com>
2026-09-14 17:34:44 +0530

    Time: 26 ms (5.14%) | Memory: 46.6 MB (24.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -10,6 +10,9 @@ class Solution {
         for(int i=0;i<s.length();i++){
             sMap.put(s.charAt(i),sMap.getOrDefault(s.charAt(i),0)+1);
             tMap.put(t.charAt(i),tMap.getOrDefault(t.charAt(i),0)+1);
+            // so basically this above does is that in a map for say "s", u will put 
+            // the char at that string then u will check how many times it appeared ,
+            //ofcourse start with zero then add +1 every time, this is what the getorDef does
         }
         return sMap.equals(tMap);
     }
