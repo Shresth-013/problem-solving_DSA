374cd0fca7e45bab7b0ad7c300c822dc28bb5300
Shresth Raj <Shresthraj12004@gmail.com>
2026-09-14 17:59:49 +0530

    Time: 5 ms (62.94%) | Memory: 44.6 MB (55.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,17 +3,17 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+        int[] freq= new int[26];
 
-        HashMap<Character, Integer> sMap=new HashMap<>();
-        HashMap<Character, Integer> tMap=new HashMap<>();
-
-        for(int i=0;i<s.length();i++){
-            sMap.put(s.charAt(i),sMap.getOrDefault(s.charAt(i),0)+1);
-            tMap.put(t.charAt(i),tMap.getOrDefault(t.charAt(i),0)+1);
-            // so basically this above does is that in a map for say "s", u will put 
-            // the char at that string then u will check how many times it appeared ,
-            //ofcourse start with zero then add +1 every time, this is what the getorDef does
+        for(char c : s.toCharArray()) {
+            freq[c-'a']++;
         }
-        return sMap.equals(tMap);
+        for(char c : t.toCharArray()){
+            freq[c-'a']--;
+        }
+        for(int f : freq){
+            if( f != 0) return false;
+        }
+        return true;
     }
 }
